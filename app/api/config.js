80f68b2d00d306fb17f1518cbd77743114b5e756
VladStD <vladstcloud@mail.ru>
2024-05-22 18:10:07 +0300

    <сообщение коммита>

--- app/api/config.js
+++ app/api/config.js
@@ -1,7 +1,7 @@
-export const BASE_URL = 'https://api-code-2.practicum-team.ru'
+export const BASE_URL = 'https://vladst-frontend.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
-  me: `${BASE_URL}/users/me`,
+  me: `${BASE_URL}/me`,
 }
